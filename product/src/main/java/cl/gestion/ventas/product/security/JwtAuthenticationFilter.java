864982024225748dd45b0e0fea6d0fb3465c4c63
Fomeke <seba.navarrozales@gmail.com>
2026-05-07 19:48:02 -0400

    feat(product): agregar validacion jwt y config

---
+++ product/src/main/java/cl/gestion/ventas/product/security/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package cl.gestion.ventas.product.security;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import cl.gestion.ventas.product.service.JwtService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filter)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filter.doFilter(request, response);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+
+        if (jwtService.isTokenValid(token)) {
+            Claims claims = jwtService.extractClaims(token);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    claims.getSubject(), null, List.of());
+            
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.info("Token valido para usuario: {}", claims.getSubject());
+        }else{
+            log.warn("Token invalido en solicitud a: {}",request.getRequestURI());
+        }
+
+        filter.doFilter(request, response);
+    }
+
+}
