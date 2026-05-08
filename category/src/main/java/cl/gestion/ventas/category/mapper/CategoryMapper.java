44f782ae8a658daa1bf1bd7c72d7612e2536e7c3
Fomeke <seba.navarrozales@gmail.com>
2026-05-08 19:49:01 -0400

    feat(category): agregar mapper

--- category/src/main/java/cl/gestion/ventas/category/mapper/CategoryMapper.java
+++ category/src/main/java/cl/gestion/ventas/category/mapper/CategoryMapper.java
@@ -1,5 +1,19 @@
 package cl.gestion.ventas.category.mapper;
 
+
+
+import org.springframework.stereotype.Component;
+
+import cl.gestion.ventas.category.dto.CategoryResponseDTO;
+import cl.gestion.ventas.category.model.Category;
+
+@Component
 public class CategoryMapper {
 
+    public CategoryResponseDTO toResponse(Category category){
+        return CategoryResponseDTO.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .build();
+    }
 }
