8c2e706937cf150e9f62c9aacbb7c59047cfaef8
Fomeke <seba.navarrozales@gmail.com>
2026-05-11 21:50:33 -0400

    feat(review): agregar metodo client y productResponse

---
+++ review/src/main/java/cl/gestion/ventas/review/client/ProductClient.java
@@ -0,0 +1,39 @@
+package cl.gestion.ventas.review.client;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import cl.gestion.ventas.review.dto.ProductResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class ProductClient {
+
+
+    @Autowired
+    private WebClient webClient;
+
+    public ProductResponse obtenerReviewPorProducto(Long productId, String token){
+        log.info("Obteniendo reviews del producto: {}", productId);
+        try{
+            return webClient.get()
+                    .uri("/{id}", productId)
+                    .header("Authorization",token)
+                    .retrieve()
+                    .bodyToMono(ProductResponse.class)
+                    .block();
+        }catch(WebClientResponseException ex){
+            log.error("Error al obtener informacion de las reviews del producto con la id: {}", productId, ex);
+            switch(ex.getStatusCode().value()){
+                case 404 -> throw new NoSuchElementException("No se encontro ese producto");
+                
+                default -> throw new RuntimeException("Error interno en el servidor");
+            }
+        }
+    }
+}
---
+++ review/src/main/java/cl/gestion/ventas/review/dto/ProductResponse.java
@@ -0,0 +1,20 @@
+package cl.gestion.ventas.review.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private String description;
+    private Long categoryId;
+}
