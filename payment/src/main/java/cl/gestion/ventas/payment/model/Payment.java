f344fc48337a092914d1f9f145c338fc4ee55dce
Fomeke <seba.navarrozales@gmail.com>
2026-05-14 17:58:29 -0400

    fix(payment): añadir anotacion @Enumerated(EnumType.STRING)

--- payment/src/main/java/cl/gestion/ventas/payment/model/Payment.java
+++ payment/src/main/java/cl/gestion/ventas/payment/model/Payment.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,14 +26,20 @@ public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "order_id",nullable = false)
     private Long orderId;
+
     @Column(nullable = false)
     private BigDecimal amount;
+
     @Column(nullable = false)
     private String paymentMethod;
+
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private PaymentStatus status;
+    
     @Column(nullable = false)
     private LocalDateTime transactionDate;
 
