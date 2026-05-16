48795ba50c8b86d14456f03fc1fd6d1ceca9e33e
pepe <rjvc97@hotmail.com>
2026-05-15 23:20:49 -0400

    fix(auth): arreglada contraseña de usuario pro

--- auth/src/main/resources/db/migration/V2__initial_data.sql
+++ auth/src/main/resources/db/migration/V2__initial_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (username, full_name,password,correo,phone) VALUES
 ('admin', 'admin gonzalez', '$2a$12$DCUAcXaneFDzpT5lL6xR4uU3Ik7JKtM7OozDfdRUvVPAQQ9F8pD4m', 'admin@duroc.cl', '+56912345678'),
 ('noob', 'noob herrera', '$2a$12$29q5RKw0g1f1CiCSL7QGNOtj410N1MWUcelcuug0O1hSGcpYReHsW', 'noob@gmail.com', '+56987654321'),
-('pro', 'pro fesional', '$2a$12$29q5RKw0g1f1CiCSL7QGNOtj410N1MWUcelcuug0O1hSGcpYReHsW', 'pro@yahoo.com', NULL);
+('pro', 'pro fesional', '$2a$12$JzRADXY9xe4Lgjuuy9mL2OnrrC88x1thTUXxbBslkuyMp9OgQ.Rf2', 'pro@yahoo.com', NULL);
