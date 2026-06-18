93c9552aa6057a3161087d362726c567a4442895
pepe <rjvc97@hotmail.com>
2026-06-18 01:08:03 -0400

    test(shipping): añadidas pruebas unitarias a servicio de envios

---
+++ shipping/src/test/java/cl/gestion/ventas/shipping/service/ShipmentServiceTest.java
@@ -0,0 +1,133 @@
+package cl.gestion.ventas.shipping.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import cl.gestion.ventas.shipping.client.OrderClient;
+import cl.gestion.ventas.shipping.dto.OrderResponseForShipping;
+import cl.gestion.ventas.shipping.dto.ShipmentRequest;
+import cl.gestion.ventas.shipping.dto.ShipmentResponse;
+import cl.gestion.ventas.shipping.mapper.ShipmentMapper;
+import cl.gestion.ventas.shipping.model.Shipment;
+import cl.gestion.ventas.shipping.repository.ShipmentRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ShipmentServiceTest {
+
+    @Mock
+    private ShipmentRepository shipmentRepository;
+
+    @Mock
+    private ShipmentMapper shipmentMapper;
+
+    @Mock
+    private OrderClient orderClient;
+
+    @InjectMocks
+    private ShipmentService shipmentService;
+
+    @Test
+    public void testObtenerEnvios(){
+        Shipment ship = new Shipment();
+        ShipmentResponse response = new ShipmentResponse();
+
+        when(shipmentRepository.findAll()).thenReturn(List.of(ship));
+        when(shipmentMapper.toResponse(ship)).thenReturn(response);
+
+        List<ShipmentResponse> resultado = shipmentService.obtenerEnvios();
+
+        assertNotNull(resultado);
+        assertEquals(1, resultado.size());
+        verify(shipmentRepository,times(1)).findAll();
+    }
+
+    @Test
+    public void testObtenerEnvioPorNumero(){
+        String num = "ABC432875";
+        Shipment ship = new Shipment();
+        ShipmentResponse response = new ShipmentResponse();
+
+        when(shipmentRepository.findByTrackingNumber(num)).thenReturn(Optional.of(ship));
+        when(shipmentMapper.toResponse(ship)).thenReturn(response);
+
+        ShipmentResponse resultado = shipmentService.obtenerEnvioPorNumero(num);
+
+        assertNotNull(resultado);
+        verify(shipmentRepository,times(1)).findByTrackingNumber(num);
+        verify(shipmentMapper,times(1)).toResponse(ship);
+        
+    }
+
+    @Test
+    public void testCrear(){
+        ShipmentRequest request = new ShipmentRequest(67L,"ABC-123","BlueExpress","Calle falsa 123");
+        Shipment ship = new Shipment();
+        Shipment savedShip = new Shipment();
+        ShipmentResponse response = new ShipmentResponse();
+        OrderResponseForShipping order = OrderResponseForShipping.builder().status("PAID").build();
+
+        when(orderClient.getOrderById(request.getOrderId())).thenReturn(order);
+        when(shipmentRepository.existsByTrackingNumber(request.getTrackingNumber())).thenReturn(false);
+        when(shipmentMapper.fromRequest(request)).thenReturn(ship);
+        when(shipmentRepository.save(ship)).thenReturn(savedShip);
+        when(shipmentMapper.toResponse(savedShip)).thenReturn(response);
+
+        ShipmentResponse resultado = shipmentService.crear(request);
+
+        assertNotNull(resultado);
+        verify(orderClient,times(1)).getOrderById(request.getOrderId());
+        verify(shipmentRepository,times(1)).existsByTrackingNumber(request.getTrackingNumber());
+        verify(shipmentRepository,times(1)).save(ship);
+    }
+
+    @Test
+    public void testEliminar(){
+        String number = "ABD-123";
+        when(shipmentRepository.existsByTrackingNumber(number)).thenReturn(true);
+        doNothing().when(shipmentRepository).deleteByTrackingNumber(number);
+
+        shipmentService.eliminar(number);
+        verify(shipmentRepository,times(1)).deleteByTrackingNumber(number);
+    }
+
+    @Test
+    public void testActualizar(){
+        String trknum = "NICE-69";
+        Long newOrderId = 10L;
+
+        ShipmentRequest request = new ShipmentRequest(newOrderId,trknum,"FEDEX","San Beka");
+        Shipment envio = Shipment.builder().trackingNumber(trknum).orderId(11L).build();
+        OrderResponseForShipping order = OrderResponseForShipping.builder().status("PAID").build();
+        Shipment envioGuardado = new Shipment();
+        ShipmentResponse response = new ShipmentResponse();
+
+        when(shipmentRepository.findByTrackingNumber(trknum)).thenReturn(Optional.of(envio));
+        when(orderClient.getOrderById(newOrderId)).thenReturn(order);
+        when(shipmentRepository.save(envio)).thenReturn(envioGuardado);
+        when(shipmentMapper.toResponse(envioGuardado)).thenReturn(response);
+
+        ShipmentResponse resultado = shipmentService.actualizar(trknum, request);
+
+        assertNotNull(resultado);
+
+        assertEquals(newOrderId, envio.getOrderId());
+        assertEquals("FEDEX", envio.getCarrier());
+        assertEquals("San Beka", envio.getShippingAddress());
+
+        verify(shipmentRepository,times(1)).findByTrackingNumber(trknum);
+        verify(orderClient,times(1)).getOrderById(newOrderId);
+        verify(shipmentRepository,times(1)).save(envio);
+    }
+}
