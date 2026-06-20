4a5154029f82dccb0a67a68edf99cdc38eab9e4b
fomeke <seba.navarrozales@gmail.com>
2026-06-20 18:18:56 -0400

    feat(payment): add junit in test package

---
+++ payment/src/test/java/cl/gestion/ventas/payment/service/PaymentServiceTest.java
@@ -0,0 +1,137 @@
+package cl.gestion.ventas.payment.service;
+
+import cl.gestion.ventas.payment.Repository.PaymentRepository;
+import cl.gestion.ventas.payment.client.OrderClient;
+import cl.gestion.ventas.payment.dto.OrderResponse;
+import cl.gestion.ventas.payment.dto.PaymentRequest;
+import cl.gestion.ventas.payment.dto.PaymentResponse;
+import cl.gestion.ventas.payment.mapper.PaymentMapper;
+import cl.gestion.ventas.payment.model.Payment;
+import cl.gestion.ventas.payment.model.PaymentStatus;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PaymentServiceTest {
+
+    @Mock
+    private PaymentRepository repo;
+
+    @Mock
+    private PaymentMapper mapper;
+
+    @Mock
+    private OrderClient client;
+
+    @InjectMocks
+    private PaymentService servi;
+
+    @Test
+    void procesarPago_Exito() {
+        
+        String token = "Bearer m1t0k3n";
+        PaymentRequest request = new PaymentRequest();
+        request.setOrderId(10L);
+        request.setAmount(new BigDecimal("50000.00"));
+
+        OrderResponse order = new OrderResponse();
+        order.setId(10L);
+        order.setStatus("PENDING"); 
+        order.setTotal(new BigDecimal("50000.00"));
+
+        Payment payment = new Payment();
+        PaymentResponse response = new PaymentResponse();
+        response.setStatus(PaymentStatus.SUCCESS);
+
+    
+        when(client.obtenerOrden(10L, token)).thenReturn(order);
+        when(mapper.fromRequest(request)).thenReturn(payment);
+        when(repo.save(payment)).thenReturn(payment);
+        when(mapper.toResponse(payment)).thenReturn(response);
+
+
+        PaymentResponse resultado = servi.procesarPago(request, token);
+
+        
+        assertEquals(PaymentStatus.SUCCESS, resultado.getStatus());
+        
+        
+        verify(client, times(1)).actualizarEstado(10L, "PAID", token);
+        verify(repo, times(1)).save(payment);
+    }
+
+    @Test
+    void procesarPago_Error_EstadoOrdenNoValido() {
+        
+        String token = "Bearer m1t0k3n";
+        PaymentRequest request = new PaymentRequest();
+        request.setOrderId(10L);
+
+        OrderResponse order = new OrderResponse();
+        order.setStatus("PAID"); 
+
+        when(client.obtenerOrden(10L, token)).thenReturn(order);
+
+        
+        IllegalArgumentException excepcion = assertThrows(IllegalArgumentException.class, () -> {
+            servi.procesarPago(request, token);
+        });
+
+        assertEquals("La orden tiene un estado no valido solo se acepta PENDIENTE (PENDING)", excepcion.getMessage());
+        
+    
+        verify(repo, never()).save(any());
+        verify(client, never()).actualizarEstado(anyLong(), anyString(), anyString());
+    }
+
+    @Test
+    void procesarPago_Error_MontoNoCoincide() {
+        
+        String token = "Bearer m1t0k3n";
+        PaymentRequest request = new PaymentRequest();
+        request.setOrderId(10L);
+        request.setAmount(new BigDecimal("30000.00")); 
+
+        OrderResponse order = new OrderResponse();
+        order.setStatus("PENDING");
+        order.setTotal(new BigDecimal("50000.00")); 
+
+        when(client.obtenerOrden(10L, token)).thenReturn(order);
+
+    
+        IllegalArgumentException excepcion = assertThrows(IllegalArgumentException.class, () -> {
+            servi.procesarPago(request, token);
+        });
+
+        assertEquals("El monto a pagar no coincide con el total de la orden.", excepcion.getMessage());
+        verify(repo, never()).save(any());
+    }
+
+    @Test
+    void eliminarPago_Error_PagoYaCompletado() {
+        
+        Long id = 1L;
+        String token = "Bearer m1t0k3n";
+        Payment payment = new Payment();
+        payment.setStatus(PaymentStatus.SUCCESS); 
+
+        when(repo.findById(id)).thenReturn(Optional.of(payment));
+
+        
+        IllegalStateException excepcion = assertThrows(IllegalStateException.class, () -> {
+            servi.eliminarPago(id, token);
+        });
+
+        assertEquals("No se puede eliminar un pago exitoso debe procesarse un reembolso.", excepcion.getMessage());
+        verify(repo, never()).delete(any());
+    }
+}
