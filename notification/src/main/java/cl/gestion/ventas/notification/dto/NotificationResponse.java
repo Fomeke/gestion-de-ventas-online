721f78b0febe7d4e0fd7cff10724da5046aee226
fomeke <seba.navarrozales@gmail.com>
2026-06-20 17:35:33 -0400

    fix(notification): add annotation in dto

--- notification/src/main/java/cl/gestion/ventas/notification/dto/NotificationResponse.java
+++ notification/src/main/java/cl/gestion/ventas/notification/dto/NotificationResponse.java
@@ -3,11 +3,15 @@ package cl.gestion.ventas.notification.dto;
 import java.time.LocalDateTime;
 
 import cl.gestion.ventas.notification.models.NotificationType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationResponse {
 
     private Long id;
