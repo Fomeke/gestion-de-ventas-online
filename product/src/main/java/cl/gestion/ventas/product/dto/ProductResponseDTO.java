c3469fb1ceb0cc01cb4744c8ec5bbe0e3db858e3
fomeke <seba.navarrozales@gmail.com>
2026-06-20 16:26:44 -0400

    fix(product):add annotation in dto

--- product/src/main/java/cl/gestion/ventas/product/dto/ProductResponseDTO.java
+++ product/src/main/java/cl/gestion/ventas/product/dto/ProductResponseDTO.java
@@ -2,10 +2,14 @@ package cl.gestion.ventas.product.dto;
 
 import java.math.BigDecimal;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class ProductResponseDTO {
 
