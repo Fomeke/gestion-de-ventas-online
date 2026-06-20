6cc6a8074395f45e2c40751a83903376c1012f42
fomeke <seba.navarrozales@gmail.com>
2026-06-20 19:05:19 -0400

    feat(category): add swagger

--- category/src/main/java/cl/gestion/ventas/category/Controller/CategoryController.java
+++ category/src/main/java/cl/gestion/ventas/category/Controller/CategoryController.java
@@ -1,23 +1,18 @@
 package cl.gestion.ventas.category.Controller;
 
 import java.util.List;
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import cl.gestion.ventas.category.dto.CategoryRequest;
 import cl.gestion.ventas.category.dto.CategoryResponseDTO;
 import cl.gestion.ventas.category.model.Category;
 import cl.gestion.ventas.category.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,22 +21,35 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequestMapping("/v1/category")
 @RequiredArgsConstructor
+@Tag(name = "Category API", description = "Endpoints para la gestion de categorias")
 public class CategoryController {
 
     private final CategoryService service;
 
+    @Operation(summary = "Obtener todas las categorias", description = "Retorna una lista con todas las categorias registradas en la base de datos.")
+    @ApiResponse(responseCode = "200", description = "Lista de categorias obtenida correctamente")
     @GetMapping
     public ResponseEntity<List<Category>> getCategoria() {
         log.info("GET api/category/buscarCategorias");
         return ResponseEntity.ok().body(service.buscarCategorias());
     }
 
+    @Operation(summary = "Buscar categoria por ID", description = "Busca y retorna los detalles de una categoria especifica.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "categoria encontrada"),
+            @ApiResponse(responseCode = "404", description = "categoria no encontrada")
+    })
     @GetMapping("/porid/{id}")
     public ResponseEntity<CategoryResponseDTO> getCategoriaPorId(@PathVariable Long id) {
         log.info("GET api/category/categoriaPorId/{}", id);
         return ResponseEntity.ok().body(service.buscarCategoriaPorId(id));
     }
 
+    @Operation(summary = "Eliminar categoria", description = "Elimina una categoria existente mediante su ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Categoria eliminada con exito (Sin contenido)"),
+            @ApiResponse(responseCode = "404", description = "Categoria no encontrada")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCategoria(@PathVariable Long id) {
         log.info("DELETE api/category/eliminarCategoria/{}", id);
@@ -49,6 +57,12 @@ public class CategoryController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Modificar categoria", description = "Actualiza los datos de una categoria existente.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Categoria actualizada correctamente"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos o nombre duplicado"),
+            @ApiResponse(responseCode = "404", description = "Categoria no encontrada")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<CategoryResponseDTO> updateCategoria(
             @PathVariable Long id,
@@ -57,6 +71,11 @@ public class CategoryController {
         return ResponseEntity.ok(service.modificarCategoria(id, category));
     }
 
+    @Operation(summary = "Crear nueva categoria", description = "Registra una nueva categoria en el sistema.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Categoria creada exitosamente"),
+            @ApiResponse(responseCode = "400", description = "Datos inválidos o la categoria ya existe")
+    })
     @PostMapping
     public ResponseEntity<CategoryResponseDTO> crearCategoria(@Valid @RequestBody CategoryRequest category){
         log.info("POST /api/category/creandoCategoria");
--- category/src/main/java/cl/gestion/ventas/category/config/SecurityConfig.java
+++ category/src/main/java/cl/gestion/ventas/category/config/SecurityConfig.java
@@ -26,7 +26,18 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/webjars/**",
+                                "/error" 
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
             return http.build();
