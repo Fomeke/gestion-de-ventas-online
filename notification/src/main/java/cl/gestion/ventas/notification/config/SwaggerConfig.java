c2fd850f9e1dba17903261a4e1c68c4cef79b742
fomeke <seba.navarrozales@gmail.com>
2026-06-21 15:23:53 -0400

    feat: add swaggerconfigs

---
+++ notification/src/main/java/cl/gestion/ventas/notification/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package cl.gestion.ventas.notification.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+    final String securitySchemeName = "BearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI().info(new Info()
+                                        .title("API de Notificaciones")
+                                        .version("1.0")
+                                        .description("API para la gestión de notificaciones"))
+                            .servers(List.of(
+                                        new Server().url("http://localhost:8088").description("Servidor local"),
+                                        new Server().url("http://localhost:8080").description("Vía API Gateway")))
+                            .components(new Components()
+                                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                    .name(securitySchemeName)
+                                    .type(SecurityScheme.Type.HTTP)
+                                    .scheme("bearer")
+                                    .bearerFormat("JWT")
+                                    .description("Ingresa directamente tu token JWT generado por el servicio de Auth.")))
+                            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
+    }
+}
---
+++ payment/src/main/java/cl/gestion/ventas/payment/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package cl.gestion.ventas.payment.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+    final String securitySchemeName = "BearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI().info(new Info()
+                                        .title("API de Pagos")
+                                        .version("1.0")
+                                        .description("API para la gestión de pagos"))
+                            .servers(List.of(
+                                        new Server().url("http://localhost:8089").description("Servidor local"),
+                                        new Server().url("http://localhost:8080").description("Vía API Gateway")))
+                            .components(new Components()
+                                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                    .name(securitySchemeName)
+                                    .type(SecurityScheme.Type.HTTP)
+                                    .scheme("bearer")
+                                    .bearerFormat("JWT")
+                                    .description("Ingresa directamente tu token JWT generado por el servicio de Auth.")))
+                            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
+    }
+}
