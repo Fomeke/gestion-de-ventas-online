4fc83e6f5512242f5161d5ddf949224eb01cdf49
pepe <rjvc97@hotmail.com>
2026-06-24 15:33:40 -0400

    fix(gateway): añadir configuracion de cors a gatewayconfig

--- api-gateway/src/main/java/cl/gestion/ventas/api_gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/cl/gestion/ventas/api_gateway/config/GatewayConfig.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
@@ -63,5 +66,33 @@ public class GatewayConfig {
                 .and(serviceRoute("ms-shipping-service-route", "shipping", "/api/v1/shipments/**"))
                 .and(docsRoute("shipping"));
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin(System.getenv()
+                .getOrDefault("ALLOWED_ORIGIN", "http://localhost:8080"));
+
+        config.addAllowedOrigin("http://127.0.0.1:8080");
+
+        config.addAllowedHeader("*");
+
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+}
 
 }
